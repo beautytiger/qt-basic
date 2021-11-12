71a7e4bb239e62570af900ff0c4d49fe7d9e81d7
wgm <konmyn@gamil.com>
2021-11-13 00:37:04 +0800

    make rect move against keypress

--- game01/game01.pro
+++ game01/game01.pro
@@ -9,9 +9,11 @@ CONFIG += c++11
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
+    MyRect.cpp \
     main.cpp
 
-HEADERS +=
+HEADERS += \
+    MyRect.h
 
 # Default rules for deployment.
 qnx: target.path = /tmp/$${TARGET}/bin
--- game01/main.cpp
+++ game01/main.cpp
@@ -1,6 +1,6 @@
 #include <QApplication>
 #include <QGraphicsScene>
-#include <QGraphicsRectItem>
+#include "MyRect.h"
 #include <QGraphicsView>
 
 int main(int argc, char *argv[])
@@ -8,9 +8,13 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
     QGraphicsScene * scene = new QGraphicsScene();
-    QGraphicsRectItem * rect = new QGraphicsRectItem();
+    MyRect * rect = new MyRect();
     rect->setRect(0,0,100,100);
     scene->addItem(rect);
+
+    rect->setFlag(QGraphicsItem::ItemIsFocusable);
+    rect->setFocus();
+
     QGraphicsView * view = new QGraphicsView(scene);
     view->show();
     return a.exec();
